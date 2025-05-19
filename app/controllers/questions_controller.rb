108f25512f97549b84bc5247b587fcf0a9fd8359
Matthieu Moloko <matthieu.moloko1506@gmail.com>
2025-05-19 14:41:52 +0200

    stupid coaching without bst

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer =
+      if @question == "I'am going to work"
+        "Great !"
+      elsif @question[-1] == "?"
+        "Silly question, get dressed and go to work!"
+
+      else @question == ""
+        "I don't care, get dressed and go to work!"
+      end
+
+    end
+end
